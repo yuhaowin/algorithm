ea3b3db18b559836bf79e782ee12ca6fa0d6b225
yuhaowin <yuhao.coding@gmail.com>
2022-04-02 22:55:28 +0800

    remove duplicate list node

---
+++ src/leetcode/editor/cn/Q83RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,89 @@
+//Given the head of a sorted linked list, delete all duplicates such that each
+//element appears only once. Return the linked list sorted as well. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,1,2]
+//Output: [1,2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,1,2,3,3]
+//Output: [1,2,3]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 300]. 
+// -100 <= Node.val <= 100 
+// The list is guaranteed to be sorted in ascending order. 
+// 
+// Related Topics 链表 👍 761 👎 0
+
+package leetcode.editor.cn;
+
+public class Q83RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new Q83RemoveDuplicatesFromSortedList().new Solution();
+        ListNode head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+        ListNode result = solution.deleteDuplicates(head);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode cur = head;
+            while (cur != null) {
+                if (cur.next != null && cur.val == cur.next.val) {
+                    cur.next = cur.next.next;
+                } else {
+                    cur = cur.next;
+                }
+            }
+            return head;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
