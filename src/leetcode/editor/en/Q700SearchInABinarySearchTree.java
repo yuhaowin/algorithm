fd8ce6c44f190737e30b7312d7cec98a94dfeca3
yuhaowin <yuhao.coding@gmail.com>
2022-04-14 18:17:29 +0800

    leet code 700 search in a binary search tree

---
+++ src/leetcode/editor/en/Q700SearchInABinarySearchTree.java
@@ -0,0 +1,59 @@
+package leetcode.editor.en;
+
+public class Q700SearchInABinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new Q700SearchInABinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val == val) {
+                return root;
+            }
+            if (val < root.val) {
+                return searchBST(root.left, val);
+            } else {
+                return searchBST(root.right, val);
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
