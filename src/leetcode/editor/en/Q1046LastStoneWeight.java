80ef81073ae6f57ec4d085af9af21c21e45c47e6
yuhaowin <yuhao.coding@gmail.com>
2022-04-14 18:00:24 +0800

    最后石头的重量

---
+++ src/leetcode/editor/en/Q1046LastStoneWeight.java
@@ -0,0 +1,33 @@
+package leetcode.editor.en;
+
+import java.util.PriorityQueue;
+
+/**
+ * 最后石头的重量
+ */
+public class Q1046LastStoneWeight {
+    public static void main(String[] args) {
+        Solution solution = new Q1046LastStoneWeight().new Solution();
+        int[] stones = new int[]{2, 7, 4, 1, 8, 1};
+        System.out.println(solution.lastStoneWeight(stones) == 1);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lastStoneWeight(int[] stones) {
+            PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+            for (int stone : stones) {
+                queue.offer(stone);
+            }
+            while (queue.size() >= 2) {
+                Integer y = queue.poll();
+                Integer x = queue.poll();
+                if (y != x) {
+                    queue.offer(y - x);
+                }
+            }
+            return queue.isEmpty() ? 0 : queue.poll();
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
