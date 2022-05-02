9fe7dc5e384574bcff330900f3dab4bb9801fe94
yuhaowin <yuhao.coding@gmail.com>
2022-05-02 19:00:02 +0800

    refactor

--- src/leetcode/editor/en/Q42TrappingRainWater.java
+++ src/leetcode/editor/en/Q42TrappingRainWater.java
@@ -1,20 +1,38 @@
-package leetcode.editor.cn;
+package leetcode.editor.en;
+
+import java.util.Stack;
 
 public class Q42TrappingRainWater {
     public static void main(String[] args) {
         Solution solution = new Q42TrappingRainWater().new Solution();
-        int[] height = new int[]{4, 2, 0, 3, 2, 5};
-        height = new int[]{5, 5, 1, 7, 1, 1, 5, 2, 7, 6};
+        int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println(solution.trap(height));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        public int trap(int[] height) {
+            int ans = 0;
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < height.length; i++) {
+                while (!stack.isEmpty() && height[stack.peek()] <= height[i]) {
+                    int index = stack.pop();
+                    int leftIndex = stack.isEmpty() ? -1 : stack.peek();
+                    if (leftIndex != -1) {
+                        int x = i - leftIndex - 1;
+                        int y = Math.min(height[leftIndex], height[i]) - height[index];
+                        ans += x * y;
+                    }
+                }
+                stack.push(i);
+            }
+            return ans;
+        }
+
         public int trap1(int[] height) {
             int length = height.length;
-            int[] left = new int[height.length];
-            int[] right = new int[height.length];
-
+            int[] left = new int[length];
+            int[] right = new int[length];
             left[0] = height[0];
             for (int i = 1; i < length; i++) {
                 if (left[i - 1] < height[i]) {
@@ -23,7 +41,6 @@ public class Q42TrappingRainWater {
                     left[i] = left[i - 1];
                 }
             }
-
             right[length - 1] = height[length - 1];
             for (int i = length - 2; i >= 0; i--) {
                 if (right[i + 1] > height[i]) {
@@ -32,30 +49,25 @@ public class Q42TrappingRainWater {
                     right[i] = height[i];
                 }
             }
-
             int result = 0;
             for (int i = 0; i < length; i++) {
                 int min = Math.min(left[i], right[i]);
-                int max = Math.max(0, min - height[i]);
-                result += max;
+                result += Math.max(0, min - height[i]);
             }
-
             return result;
         }
 
-        public int trap(int[] height) {
+        public int trap2(int[] height) {
             int length = height.length;
             if (length < 3) {
                 return 0;
             }
-
             int L = 0;
             int R = length - 1;
-            int result = 0;
-
             int leftMax = height[0];
             int rightMax = height[length - 1];
 
+            int result = 0;
             while (L <= R) {
                 if (leftMax <= rightMax) {
                     result += Math.max(0, leftMax - height[L]);
