e639fa0407a0c9f683798e62a4a629f59c5399d3
yuhaowin <yuhao.coding@gmail.com>
2022-05-07 14:26:43 +0800

    leet code 876

--- src/leetcode/editor/en/Q876MiddleOfTheLinkedList.java
+++ src/leetcode/editor/en/Q876MiddleOfTheLinkedList.java
@@ -1,26 +1,21 @@
-package leetcode.editor.cn;
+package leetcode.editor.en;
 
 public class Q876MiddleOfTheLinkedList {
     public static void main(String[] args) {
         Solution solution = new Q876MiddleOfTheLinkedList().new Solution();
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        System.out.println(solution.middleNode(head).val);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     * int val;
-     * ListNode next;
-     * ListNode() {}
-     * ListNode(int val) { this.val = val; }
-     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
     class Solution {
         public ListNode middleNode(ListNode head) {
+            if (head == null) {
+                return null;
+            }
             ListNode slow = head;
             ListNode fast = head;
+
             while (fast != null && fast.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
@@ -30,6 +25,9 @@ public class Q876MiddleOfTheLinkedList {
     }
     //leetcode submit region end(Prohibit modification and deletion)
 
+    /**
+     * Definition for singly-linked list.
+     */
     static class ListNode {
         int val;
         ListNode next;
